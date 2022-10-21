c416aab8cffac30b937433910aeb674479fccfb9
boyetech111 <97601467+boyetech111@users.noreply.github.com>
2022-10-21 14:21:47 +0100

    mega

--- pages/ourwork.jsx
+++ pages/ourwork.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from '../component/Navbar';
 import Workfooter from '../component/Workfooter';
-import Workheader from '../component/workheader';
+import Workheader from '../component/Workheader';
 import Workproduct from '../component/Workproduct';
 
 
